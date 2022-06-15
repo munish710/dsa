59438cb3cc26adea9f9f756d09283fb8dbb631d7
munish710 <munish710@gmail.com>
2022-06-16 00:39:28 +0530

    sorting : add merge sort by updating the input array

--- sorting/mergeSort.js
+++ sorting/mergeSort.js
@@ -42,4 +42,63 @@ function merge(arr1, arr2) {
 }
 
 // console.log(merge([1, 2, 3, 6, 8], [2, 7, 9, 10]));
-console.log(mergeSort([1, 4, 0, 3, 11, 99, 80]));
+// console.log(mergeSort([1, 4, 0, 3, 11, 99, 80]));
+
+function mergeSort2(arr, left, right) {
+  if (left >= right) {
+    return;
+  }
+  let middle = Math.floor((left + right) / 2);
+  mergeSort2(arr, left, middle);
+  mergeSort2(arr, middle + 1, right);
+
+  merge2(arr, left, middle, right);
+}
+
+function merge2(arr, left, middle, right) {
+  let m = middle - left + 1;
+  let n = right - middle;
+
+  let leftArr = new Array(m);
+  let rightArr = new Array(n);
+
+  for (let i = 0; i < m; i++) {
+    leftArr[i] = arr[left + i];
+  }
+
+  for (let j = 0; j < n; j++) {
+    rightArr[j] = arr[middle + 1 + j];
+  }
+
+  let i = 0,
+    j = 0,
+    k = left;
+
+  while (i < m && j < n) {
+    if (leftArr[i] < rightArr[j]) {
+      arr[k] = leftArr[i];
+      i++;
+    } else {
+      arr[k] = rightArr[j];
+      j++;
+    }
+    k++;
+  }
+
+  while (i < m) {
+    arr[k] = leftArr[i];
+    i++;
+    k++;
+  }
+
+  while (j < n) {
+    arr[k] = rightArr[j];
+    j++;
+    k++;
+  }
+}
+
+const arr = [12, 11, 13, 5, 6, 7];
+// merge2(arr, 0, 3, arr.length - 1);
+mergeSort2(arr, 0, arr.length - 1);
+console.log(arr);
