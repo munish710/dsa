6ef035be74611a8d26a269f3dced09378543a137
munish710 <munish710@gmail.com>
2022-06-23 23:56:49 +0530

    data-structures : add SignlyLinkedList

---
+++ data-structures/singly-linked-list/SinglyLinkedList.js
@@ -0,0 +1,61 @@
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+class SinglylinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  push(val) {
+    let newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
+
+  pop() {
+    if (!this.head) {
+      return undefined;
+    }
+    let current = this.head;
+    let newTail = current;
+    while (current.next) {
+      newTail = current;
+      current = current.next;
+    }
+    this.tail = newTail;
+    this.tail.next = null;
+    this.length--;
+
+    if (this.length === 0) {
+      this.head = null;
+      this.tail = null;
+    }
+
+    return current;
+  }
+}
+
+const list = new SinglylinkedList();
+
+list.push(231);
+list.push(24);
+list.push(25);
+list.push(26);
+
+console.log(list);
+
+list.pop();
+console.log(list);
