c13baeb8459672e52b4efb4aedaee30235e9b4f2
munish710 <munish710@gmail.com>
2022-07-10 20:22:34 +0530

    doublyLinkedList : add aditional methods

--- data-structures/doubly-linked-list/DoublyLinkedList.js
+++ data-structures/doubly-linked-list/DoublyLinkedList.js
@@ -43,6 +43,124 @@ class DoublyLinkedList {
     this.length--;
     return poppedNode;
   }
+
+  shift() {
+    if (!this.head) {
+      return undefined;
+    }
+    let removedNode = this.head;
+    if (this.head === this.tail) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.head = removedNode.next;
+      this.head.prev = null;
+      removedNode.next = null;
+    }
+
+    this.length--;
+    return removedNode;
+  }
+
+  unshift(val) {
+    let newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      newNode.next = this.head;
+      this.head.prev = newNode;
+      this.head = newNode;
+    }
+    this.length++;
+    return this;
+  }
+
+  get(index) {
+    if (index < 0 || index >= this.length) {
+      return null;
+    }
+    let middle = Math.floor(this.length / 2);
+    let counter;
+    let current;
+    if (index <= middle) {
+      current = this.head;
+      counter = 0;
+      while (counter !== index) {
+        current = current.next;
+        counter++;
+      }
+    } else {
+      counter = this.length - 1;
+      current = this.tail;
+      while (counter !== index) {
+        current = current.prev;
+        counter--;
+      }
+    }
+
+    return current;
+  }
+
+  set(index, val) {
+    let foundNode = this.get(index);
+    if (foundNode) {
+      foundNode.val = val;
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  insertAt(index, val) {
+    if (index < 0 || index > this.length) {
+      return false;
+    }
+
+    if (index === 0) {
+      this.unshift(val);
+      return true;
+    }
+
+    if (index === this.length) {
+      this.push(val);
+      return true;
+    }
+    let newNode = new Node(val);
+    let beforeNode = this.get(index - 1);
+    let afterNode = beforeNode.next;
+    afterNode.prev = newNode;
+    newNode.next = afterNode;
+    newNode.prev = beforeNode;
+    beforeNode.next = newNode;
+    this.length++;
+    return true;
+  }
+
+  removeAt(index) {
+    if (index < 0 || index >= this.length) {
+      return undefined;
+    }
+    if (index === 0) {
+      return this.shift();
+    }
+    if (index === this.length - 1) {
+      return this.pop();
+    }
+
+    let foundNode = this.get(index);
+    let beforeNode = foundNode.prev;
+    let afterNode = foundNode.next;
+    beforeNode.next = afterNode;
+    afterNode.prev = beforeNode;
+
+    foundNode.next = null;
+    foundNode.prev = null;
+    return foundNode;
+  }
 }
 
 const list = new DoublyLinkedList();
+list.push(1);
+list.push(2);
+list.push(3);
