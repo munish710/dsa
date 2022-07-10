24d195ed29a295ecf52ce9e8410f78fedba57183
munish710 <munish710@gmail.com>
2022-07-10 18:43:29 +0530

    data-strutctures : add doublyLinkedList

---
+++ data-structures/doubly-linked-list/DoublyLinkedList.js
@@ -0,0 +1,48 @@
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+    this.prev = null;
+  }
+}
+
+class DoublyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  push(val) {
+    let newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      this.tail.next = newNode;
+      newNode.prev = this.tail;
+      this.tail = newNode;
+    }
+    this.length++;
+  }
+
+  pop() {
+    if (this.length === 0) {
+      return undefined;
+    }
+
+    let poppedNode = this.tail;
+    if (this.head === this.tail) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.tail = poppedNode.prev;
+      this.tail.next = null;
+      poppedNode.prev = null;
+    }
+    this.length--;
+    return poppedNode;
+  }
+}
+
+const list = new DoublyLinkedList();
